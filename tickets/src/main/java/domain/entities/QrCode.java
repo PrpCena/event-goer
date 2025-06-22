03e235dda02e5486ebb6ffb03e1c48eeeea5a30d
prparmar1 <161976176+PrpCena@users.noreply.github.com>
2025-06-22 12:30:07 -0400

    add QrCode entity

--- tickets/src/main/java/domain/entities/QrCode.java
+++ tickets/src/main/java/domain/entities/QrCode.java
@@ -1,26 +1,22 @@
 package domain.entities;
 
-
-import domain.TicketStatusEnum;
+import domain.QrCodeStatusEnum;
 import jakarta.persistence.*;
 import lombok.*;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.UUID;
 
 @Entity
+@AllArgsConstructor
+@NoArgsConstructor
 @Getter
 @Setter
-@NoArgsConstructor
-@AllArgsConstructor
 @Builder
-@Table(name = "tickets")
-public class Ticket {
-  
+@Table(name = "qr_codes")
+public class QrCode {
   @Id
   @Column(name = "id", nullable = false, updatable = false)
   @GeneratedValue(strategy = GenerationType.UUID)
@@ -28,21 +24,14 @@ public class Ticket {
   
   @Column(name = "status", nullable = false)
   @Enumerated(EnumType.STRING)
-  private TicketStatusEnum status;
+  private QrCodeStatusEnum status;
+  
+  @Column(name = "value", nullable = false)
+  private String value;
   
   @ManyToOne(fetch = FetchType.LAZY)
-  @JoinColumn(name = "ticket_type_id")
-  private TicketType ticketType;
-  
-  @ManyToOne(fetch = FetchType.LAZY)
-  @JoinColumn(name = "purchaser_id")
-  private User purchaser;
-  
-  @OneToMany(mappedBy = "ticket", cascade = CascadeType.ALL)
-  private List<TicketValidation> validations = new ArrayList<>();
-  
-  @OneToMany(mappedBy = "ticket", cascade = CascadeType.ALL)
-  private List<QrCode> qrCodes = new ArrayList<>();
+  @JoinColumn(name = "ticket_id")
+  private Ticket ticket;
   
   @CreatedDate
   @Column(name = "created_at", updatable = false, nullable = false)
