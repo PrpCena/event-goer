ca82f6c5cb6d6b51b8cdcd136f916902cc7f034c
prparmar1 <161976176+PrpCena@users.noreply.github.com>
2025-06-22 18:15:29 -0400

    implement create event end point

---
+++ tickets/src/main/java/com/prp/tickets/controller/EventController.java
@@ -0,0 +1,38 @@
+package com.prp.tickets.controller;
+
+
+import com.prp.tickets.domain.CreateEventRequest;
+import com.prp.tickets.domain.dto.CreateEventRequestDto;
+import com.prp.tickets.domain.dto.CreateEventResponseDto;
+import com.prp.tickets.domain.entities.Event;
+import com.prp.tickets.mappers.EventMapper;
+import com.prp.tickets.services.EventService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping(path = "/api/events")
+public class EventController {
+  
+  final EventService eventService;
+  final EventMapper eventMapper;
+  
+  @PostMapping
+  public ResponseEntity<CreateEventResponseDto> createEvent(@RequestBody @Valid CreateEventRequestDto createEventRequestDto, @AuthenticationPrincipal Jwt jwt) {
+	CreateEventRequest createEventRequest = eventMapper.fromDto(createEventRequestDto);
+	UUID userId = UUID.fromString(jwt.getSubject());
+	Event event = eventService.createEvent(userId, createEventRequest);
+	CreateEventResponseDto toDto = eventMapper.toDto(event);
+	return new  ResponseEntity<>(toDto, HttpStatus.CREATED);
+  }
+  
+  
+}
