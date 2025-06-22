37af82fe3bf84582fbe5a77283bcf26bf772a83e
prparmar1 <161976176+PrpCena@users.noreply.github.com>
2025-06-22 12:40:11 -0400

    add hashCode and equals methods to the entities

--- tickets/src/main/java/domain/entities/TicketType.java
+++ tickets/src/main/java/domain/entities/TicketType.java
@@ -9,6 +9,7 @@ import org.springframework.data.annotation.LastModifiedDate;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.UUID;
 
 @Entity
@@ -39,13 +40,26 @@ public class TicketType {
   
   @OneToMany(mappedBy = "ticketType", cascade = CascadeType.ALL)
   private List<Ticket> tickets = new ArrayList<>();
-  
   // TODO: Tickets
   @CreatedDate
   @Column(name = "created_at", updatable = false, nullable = false)
   private LocalDateTime createdAt;
-  
   @LastModifiedDate
   @Column(name = "updated_at", nullable = false)
   private LocalDateTime updatedAt;
+  
+  @Override
+  public boolean equals(Object o) {
+	if (o == null || getClass() != o.getClass())
+	  return false;
+	TicketType that = (TicketType) o;
+	return Objects.equals(id, that.id) && Objects.equals(name, that.name) && Objects.equals(
+	  price, that.price) && Objects.equals(totalAvailable, that.totalAvailable) && Objects.equals(
+	  createdAt, that.createdAt) && Objects.equals(updatedAt, that.updatedAt);
+  }
+  
+  @Override
+  public int hashCode() {
+	return Objects.hash(id, name, price, totalAvailable, createdAt, updatedAt);
+  }
 }
